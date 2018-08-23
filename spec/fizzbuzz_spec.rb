6c0ce11ab7da7b8407b6ec4051925e4c09bc6f59
Laura Jackson <playeronereadystart@gmail.com>
2018-08-23 15:13:55 +0100

    Return "fizz" if number given to fizzbuzz is a multiple of 3

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -8,6 +8,9 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
   it 'returns given number if it is not a 3 or 5' do
-    expect(fizzbuzz(9)).to eq 9
+    expect(fizzbuzz(2)).to eq 2
+  end
+  it 'return "fizz" if number is divisible by 3' do
+    expect(fizzbuzz(30)).to eq "fizz"
   end
 end
