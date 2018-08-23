3f829d7486eb6d862561bca5f56416bcd1059167
Laura Jackson <playeronereadystart@gmail.com>
2018-08-23 16:23:54 +0100

    Returns 'buzz' if number is divisible by 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -10,7 +10,12 @@ describe 'fizzbuzz' do
   it 'returns given number if it is not a 3 or 5' do
     expect(fizzbuzz(2)).to eq 2
   end
-  it 'return "fizz" if number is divisible by 3' do
+  it 'returns "fizz" if number is divisible by 3' do
     expect(fizzbuzz(30)).to eq "fizz"
+    expect(fizzbuzz(9)).to eq "fizz"
+  end
+  it 'returns "buzz" if number is divisible by 5' do
+    expect(fizzbuzz(50)).to eq "buzz"
+    expect(fizzbuzz(25)).to eq "buzz"
   end
 end
